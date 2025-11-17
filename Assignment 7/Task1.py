11d4b4c13ce502f4d041f300b0dd53aed72e4b30
manoj93906 <manojguniganti347@gmail.com>
2025-11-17 14:43:52 +0530

    Create Task1.py

---
+++ Assignment 7/Task1.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+# Taking only user inputs
+a = int(input("Enter value for a: "))
+b = int(input("Enter value for b: "))
+
+# Displaying sum
+result = add(a, b)
+print("The sum is:", result)
